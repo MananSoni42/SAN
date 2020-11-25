0473052db064628599515f39b83e68a342c0f887
Manan Soni <manansoni1399@gmail.com>
2020-11-25 12:19:08 +0530

    commit final plots and results

--- plot_results.py
+++ plot_results.py
@@ -19,14 +19,14 @@ datas = ['train', 'dev']
 if args.clear:
     os.system('gio trash plots/*.jpg')
 
-#### Joint SAN ####
+#### SAN ####
 if not args.san_off:
     if args.lambda_comp:
         # Plot em, f1 of joint_san for different lambdas
         for dataset in datas:
-            for i,metric in enumerate([f'{dataset}_acc', f'{dataset}_em', f'{dataset}_f1']):
+            for i,metric in enumerate([f'{dataset}_em', f'{dataset}_f1']):
                 plt.gcf().clear()
-                plt.title(f' dev {metric.split("_")[-1]} for Joint SAN + classifier')
+                plt.title(f' {dataset} {metric.split("_")[-1]} for only SAN')
                 plt.xlabel('epochs')
                 plt.gca().set_ylim([0,100])
                 for lval in lambda_vals:
@@ -44,10 +44,8 @@ if not args.san_off:
         plt.xlabel('epochs')
         train_loss = data['train_loss_san'] + lval*data['train_loss_class']
         plt.plot(train_loss, label='total loss')
-        plt.plot(data['train_loss_san'], label='san loss')
-        plt.plot(data['train_loss_class'], label='classifier loss')
         plt.legend(loc='upper left')
-        plt.title(f'Training loss for Joint SAN + classifier, lambda = {lval}')
+        plt.title(f'Training loss for only SAN, lambda = {lval}')
         plt.savefig(f'plots/js_train_loss_{lval}.jpg')
         if args.show:
             plt.show()
@@ -60,7 +58,7 @@ if not args.san_class_off:
         for dataset in datas:
             for i,metric in enumerate([f'{dataset}_acc', f'{dataset}_em', f'{dataset}_f1']):
                 plt.gcf().clear()
-                plt.title(f' dev {metric.split("_")[-1]} for Joint SAN + classifier')
+                plt.title(f' {dataset} {metric.split("_")[-1]} for Joint SAN + classifier')
                 plt.xlabel('epochs')
                 plt.gca().set_ylim([0,100])
                 for lval in lambda_vals:
